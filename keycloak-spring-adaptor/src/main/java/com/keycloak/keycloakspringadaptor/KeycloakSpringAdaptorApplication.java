1d572f78e27b81096e2026e3c5a5d3c111a23a1d
kolsi-omar <omar.kolsi@nearteam.fr>
2022-06-08 18:00:01 +0100

    project first commit

---
+++ keycloak-spring-adaptor/src/main/java/com/keycloak/keycloakspringadaptor/KeycloakSpringAdaptorApplication.java
@@ -0,0 +1,13 @@
+package com.keycloak.keycloakspringadaptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakSpringAdaptorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeycloakSpringAdaptorApplication.class, args);
+    }
+
+}
---
+++ keycloak-spring-adaptor/src/test/java/com/keycloak/keycloakspringadaptor/KeycloakSpringAdaptorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keycloak.keycloakspringadaptor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakSpringAdaptorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
